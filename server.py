d463f1e30ed7b6123f3c2073e759a1700d462ebf
Pavan <spavansdr@gmail.com>
2025-07-28 09:24:11 +0530

    oneee message

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-import time
 
-def main():
-    secret = os.getenv("MYSECRET", "no-secret")
-    print(f"Using secret: {secret}")
-    time.sleep(5)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        secret = os.environ.get("MYSECRET", "Secret not found")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(f"Simple Python Demo Project: {secret}".encode())
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 8000))
+    HTTPServer(("", port), Handler).serve_forever()
