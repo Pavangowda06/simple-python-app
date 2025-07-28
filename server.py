3a4c88561a5043f8d1c89f110c2bda9cecfedff6
Pavan <spavansdr@gmail.com>
2025-07-28 09:08:12 +0530

    oneee message

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 import time
 
 def main():
-    secret = os.getenv("MYSECRET", "no-secrets")
+    secret = os.getenv("MYSECRET", "no-secret")
     while True:
         print(f"Working... Using secret: {secret}")
         time.sleep(10)
